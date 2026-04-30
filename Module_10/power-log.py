8f74bdcf35926715edf1b19bae2e94f3060dee43
John Abbah <johnabbah999@gmail.com>
2026-04-30 20:38:07 +0100

    Updated CRUD in power-log.py

--- Module_10/power-log.py
+++ Module_10/power-log.py
@@ -20,7 +20,7 @@ def addReadings() :
 
     power = voltage * current
 
-    remote.execute("INSERT INTO Power_log (voltage,current, power) VALUES(?, ?,?)",(voltage, current, power))
+    remote.execute("INSERT INTO Power_log (voltage,current, power) VALUES(?, ?, ?)",(voltage, current, power))
     link.commit()
     print("Readings recorded. Power =",power,"W")
 
@@ -31,6 +31,18 @@ def viewReadings() :
     for row in rows :
         print(row)
 
+def updateReadings() :
+    recordId = int(input("Input ID: "))
+    newVoltage = float(input("Input voltage: "))
+    newCurrent = float(input("input current: "))
+
+    newPower = newVoltage * newCurrent
+    remote.execute("""
+     UPDATE Power_log SET voltage = ?, current = ?, voltage = ?
+     WHERE id = ?""", (newVoltage, newCurrent, newPower, recordId))
+    link.commit()
+    print("Update Complete")
+
 def deleteRecord() :
     idRecord = int(input("Input record id: "))
     remote.execute("DELETE FROM Power_log WHERE id = ?", (idRecord,))
@@ -40,8 +52,9 @@ def deleteRecord() :
 while True:
     print("\n1. Add readings")
     print("2. View readings")
-    print("3. Delete readings")
-    print("4. Exit")
+    print("3. update readings")
+    print("4. Delete readings")
+    print("5. Exit")
 
     option = int(input("\nEnter option: "))
     if option == 1 :
@@ -49,9 +62,11 @@ while True:
     elif option == 2 :
         viewReadings()
     elif option == 3 :
-        deleteRecord()
+        updateReadings()
     elif option == 4 :
-        break;
+        deleteRecord()
+    elif option == 5 :
+        break
     else :
      print("\nInvalid option")
 
